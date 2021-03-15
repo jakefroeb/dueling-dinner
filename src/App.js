a99ac03f6a1d7c318ab05d2ed1f047a62829deb1
jakefroeb <jake.herman.froeb@gmail.com>
2021-03-15 14:11:51 -0500

    auth setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Redirect } from "react-router-dom"
+import { Login } from "./components/auth/Login"
+import { Register } from "./components/auth/Register"
+import { userStorageKey } from "./components/auth/authSettings"
 
 function App() {
   return (
@@ -7,6 +10,24 @@ function App() {
       <header className="duelingDinner-header">
         Dueling Dinner
       </header>
+      <Route render={() => {
+        if (sessionStorage.getItem(userStorageKey)) {
+          return (
+            <>
+              //Components that are rendered when the user is authenticated go inside this React fragment
+            </>
+          )
+        } else {
+          return <Redirect to="/login" />;
+        }
+    }} />
+
+    <Route path="/login">
+      <Login />
+    </Route>
+    <Route path="/register">
+      <Register />
+    </Route>
     </div>
   );
 }
